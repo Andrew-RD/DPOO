96a05ffe70e513bb3c8bbafdf48a41390e7e0aeb
liber <liber@AnaPc.linksys.cache>
2025-07-14 15:06:41 -0400

    Creando atributos de la Clase OfertaLaboral

--- BolsaLaboral/src/logico/OfertaLaboral.java
+++ BolsaLaboral/src/logico/OfertaLaboral.java
@@ -1,9 +1,18 @@
 package logico;
 
 public class OfertaLaboral {
+	private String nombreEmpresa;
 
 	public OfertaLaboral() {
 		// TODO Auto-generated constructor stub
+	}
+
+	public String getNombreEmpresa() {
+		return nombreEmpresa;
+	}
+
+	public void setNombreEmpresa(String nombreEmpresa) {
+		this.nombreEmpresa = nombreEmpresa;
 	}
 
 }
