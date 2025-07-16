41222b59224723f8073cc32c82a41a015526e394
liber <liber@AnaPc.linksys.cache>
2025-07-16 17:41:57 -0400

    Declaracion de clases hijas de Candidato

---
+++ BolsaLaboral/src/logico/Universitario.java
@@ -0,0 +1,46 @@
+package logico;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Universitario extends Candidato {
+	private String universidad;
+	private String carrera;
+	private String nivelAcademico;
+	
+	public Universitario(String codigo, String identificacion, String nombres, String apellidos,
+			LocalDate fechaNacimiento, String provincia, String municipio, String telefono, String correo,
+			String jornada, String modalidad, float aspiracionSalarial, boolean licenciaConducir,
+			boolean disposicionMudarse, ArrayList<String> idiomas, ArrayList<Solicitud> misSolicitudes,
+			String universidad, String carrera, String nivelAcademico) {
+		super(codigo, identificacion, nombres, apellidos, fechaNacimiento, provincia, municipio, telefono, correo,
+				jornada, modalidad, aspiracionSalarial, licenciaConducir, disposicionMudarse, idiomas, misSolicitudes);
+		this.universidad = universidad;
+		this.carrera = carrera;
+		this.nivelAcademico = nivelAcademico;
+	}
+
+	public String getUniversidad() {
+		return universidad;
+	}
+
+	public void setUniversidad(String universidad) {
+		this.universidad = universidad;
+	}
+
+	public String getCarrera() {
+		return carrera;
+	}
+
+	public void setCarrera(String carrera) {
+		this.carrera = carrera;
+	}
+
+	public String getNivelAcademico() {
+		return nivelAcademico;
+	}
+
+	public void setNivelAcademico(String nivelAcademico) {
+		this.nivelAcademico = nivelAcademico;
+	}
+}
